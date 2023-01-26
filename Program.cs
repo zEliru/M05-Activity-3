945ea61db49c86fcf820ae9bd2081eafab5837a1
Edu <pewpet@gmail.com>
2023-01-26 15:24:37 +0100

    Topic 1 done. Topic 2 and 3 are still WIP

--- Program.cs
+++ Program.cs
@@ -42,8 +42,8 @@
         public static void ShowMenu() {
             Console.Clear();
             Console.WriteLine("Welcome to the main menu");
-            Console.WriteLine("1 - Topic 1");
-            Console.WriteLine("2 - Topic 2");
+            Console.WriteLine("1 - Colors");
+            Console.WriteLine("2 - Food");
             Console.WriteLine("3 - Topic 3");
             Console.WriteLine("4 - Topic 4");
             Console.WriteLine("5 - Topic 5");
@@ -58,7 +58,7 @@
         public static void TopicOneQuestions()
         {
             Console.Clear();
-            Console.WriteLine("Hi, welcome to Topic One XD.");
+            Console.WriteLine("Hi, welcome to topic one: Colors.");
             bool valid = false;
             while (!valid)
             {
@@ -66,27 +66,27 @@
                 {
                     int choiceFromUser = 0;                    
                     Console.WriteLine("Question 1: What's my favorite color?");
-                    Console.WriteLine("1 - ");
-                    Console.WriteLine("2 - ");
-                    Console.WriteLine("3 - ");
-                    Console.WriteLine("4 - ");
+                    Console.WriteLine("1 - Red");
+                    Console.WriteLine("2 - Green");
+                    Console.WriteLine("3 - Blue");
+                    Console.WriteLine("4 - Edu");
                     choiceFromUser = Convert.ToInt32(Console.ReadLine());
                     switch (choiceFromUser)
                     {
                         case 1:
-                            Console.WriteLine("");
+                            Console.WriteLine("Wrong.");
                             valid = true;
                             break;
                         case 2:
-                            Console.WriteLine("");
+                            Console.WriteLine("Wrong");
                             valid = true;
                             break;
                         case 3:
-                            Console.WriteLine("");
+                            Console.WriteLine("Wrong");
                             valid = true;
                             break;
                         case 4:
-                            Console.WriteLine("");
+                            Console.WriteLine("Correct.");
                             valid = true;
                             break;
                         default:
@@ -104,34 +104,34 @@
 
             }
             valid = false;
-            Console.WriteLine("Answer submitted.");
+            Console.WriteLine("Answer properly processed.");
             while (!valid)
             {
                 try
                 {
                     int choiceFromUser = 0;
-                    Console.WriteLine("Question 1: What's my favorite color?");
-                    Console.WriteLine("1 - ");
-                    Console.WriteLine("2 - ");
-                    Console.WriteLine("3 - ");
-                    Console.WriteLine("4 - ");
+                    Console.WriteLine("Question 2: What's YOUR favorite color?");
+                    Console.WriteLine("1 - Edu");
+                    Console.WriteLine("2 - Edu");
+                    Console.WriteLine("3 - Not Edu");
+                    Console.WriteLine("4 - Edu");
                     choiceFromUser = Convert.ToInt32(Console.ReadLine());
                     switch (choiceFromUser)
                     {
                         case 1:
-                            Console.WriteLine("");
+                            Console.WriteLine("Wrong");
                             valid = true;
                             break;
                         case 2:
-                            Console.WriteLine("");
+                            Console.WriteLine("Wrong");
                             valid = true;
                             break;
                         case 3:
-                            Console.WriteLine("");
+                            Console.WriteLine("Correct");
                             valid = true;
                             break;
                         case 4:
-                            Console.WriteLine("");
+                            Console.WriteLine("Wrong");
                             valid = true;
                             break;
                         default:
@@ -149,34 +149,34 @@
 
             }
             valid = false;
-            Console.WriteLine("Answer submitted.");
+            Console.WriteLine("Answer properly processed.");
             while (!valid)
             {
                 try
                 {
                     int choiceFromUser = 0;
-                    Console.WriteLine("Question 1: What's my favorite color?");
-                    Console.WriteLine("1 - ");
-                    Console.WriteLine("2 - ");
-                    Console.WriteLine("3 - ");
-                    Console.WriteLine("4 - ");
+                    Console.WriteLine("Question 3: What color fits me best?");
+                    Console.WriteLine("1 - Black");
+                    Console.WriteLine("2 - White");
+                    Console.WriteLine("3 - Yellow");
+                    Console.WriteLine("4 - Purple");
                     choiceFromUser = Convert.ToInt32(Console.ReadLine());
                     switch (choiceFromUser)
                     {
                         case 1:
-                            Console.WriteLine("");
+                            Console.WriteLine("Correct");
                             valid = true;
                             break;
                         case 2:
-                            Console.WriteLine("");
+                            Console.WriteLine("Wrong");
                             valid = true;
                             break;
                         case 3:
-                            Console.WriteLine("");
+                            Console.WriteLine("Wrong");
                             valid = true;
                             break;
                         case 4:
-                            Console.WriteLine("");
+                            Console.WriteLine("Wrong");
                             valid = true;
                             break;
                         default:
@@ -194,34 +194,34 @@
 
             }
             valid = false;
-            Console.WriteLine("Answer submitted.");
+            Console.WriteLine("Answer properly processed.");
             while (!valid)
             {
                 try
                 {
                     int choiceFromUser = 0;
-                    Console.WriteLine("Question 1: What's my favorite color?");
-                    Console.WriteLine("1 - ");
-                    Console.WriteLine("2 - ");
-                    Console.WriteLine("3 - ");
-                    Console.WriteLine("4 - ");
+                    Console.WriteLine("Question 4: When blod dries up, what color does it take?");
+                    Console.WriteLine("1 - Brown");
+                    Console.WriteLine("2 - Green");
+                    Console.WriteLine("3 - It stays red you idiot");
+                    Console.WriteLine("4 - Does it even have a color");
                     choiceFromUser = Convert.ToInt32(Console.ReadLine());
                     switch (choiceFromUser)
                     {
                         case 1:
-                            Console.WriteLine("");
+                            Console.WriteLine("Correct, how did you know though");
                             valid = true;
                             break;
                         case 2:
-                            Console.WriteLine("");
+                            Console.WriteLine("Wrong");
                             valid = true;
                             break;
                         case 3:
-                            Console.WriteLine("");
+                            Console.WriteLine("Wrong and please don't insult me anymore I have FEELINGS :(");
                             valid = true;
                             break;
                         case 4:
-                            Console.WriteLine("");
+                            Console.WriteLine("Yes... obviously... anyways, wrong answer");
                             valid = true;
                             break;
                         default:
@@ -239,34 +239,34 @@
 
             }
             valid = false;
-            Console.WriteLine("Answer submitted.");
+            Console.WriteLine("Answer properly processed.");
             while (!valid)
             {
                 try
                 {
                     int choiceFromUser = 0;
-                    Console.WriteLine("Question 1: What's my favorite color?");
-                    Console.WriteLine("1 - ");
-                    Console.WriteLine("2 - ");
-                    Console.WriteLine("3 - ");
-                    Console.WriteLine("4 - ");
+                    Console.WriteLine("Question 5: What are colors made of?");
+                    Console.WriteLine("1 - Colors");
+                    Console.WriteLine("2 - It's all about the light and how it hits objects");
+                    Console.WriteLine("3 - Pencils");
+                    Console.WriteLine("4 - To be or not to be");
                     choiceFromUser = Convert.ToInt32(Console.ReadLine());
                     switch (choiceFromUser)
                     {
                         case 1:
-                            Console.WriteLine("");
+                            Console.WriteLine("Wrong");
                             valid = true;
                             break;
                         case 2:
-                            Console.WriteLine("");
+                            Console.WriteLine("Wrong");
                             valid = true;
                             break;
                         case 3:
-                            Console.WriteLine("");
+                            Console.WriteLine("Wrong");
                             valid = true;
                             break;
                         case 4:
-                            Console.WriteLine("");
+                            Console.WriteLine("That is, indeed, the question");
                             valid = true;
                             break;
                         default:
@@ -284,7 +284,8 @@
 
             }
             valid = false;
-            Console.WriteLine("Answer submitted.");
+            Console.WriteLine("Thank you for participating. Press any key to go to the menu.");
+            Console.ReadKey();
 
             ShowMenu();
 
